fcab9b189f4c894e3b2d7c69f33c0172211c8bd4
swetha <ramyaragu407@gmail.com>
2022-05-31 18:22:28 +0530

    Second commit

--- Myprogram.java
+++ Myprogram.java
@@ -1,5 +1,7 @@
 class MyProgram{
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Changes to commit");
+        System.out.println("changes to second commit");
         }
     }
